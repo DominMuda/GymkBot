c7118a926744800c141394f632d15a5ae9786b51
DominMuda <dmunnoz96@gmail.com>
2016-08-04 19:07:50 +0200

    Preaparing for testing

--- GymkBot.py
+++ GymkBot.py
@@ -2,11 +2,8 @@
 from datetime import date, datetime
 from subprocess import call
 import telebot, os.path
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import InlineQueryHandler
 import logging
+import configparser
 import io
 
 
@@ -14,15 +11,25 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-updater = Updater(token='269890633:AAHaYy-FkcZwm1ooxtMmfTJ3yIhjbnfNe2E')
-bool_list = [False,False,False,False,False,False,False,False,False,False]
+pruebas_disponibles = []
+bool_list = []
+for x in range(0,10):
+	bool_list.insert(x,False)
+	pruebas_disponibles.insert(x, "/prueba" + str(x + 1))
 
-dispatcher = updater.dispatcher
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+token = config['Bot']['token']
+chat_id = config['Admin']['chat_id']
+bot = telebot.TeleBot(token, skip_pending=True)
+
+def create_DB():
+	self.conn = sqlite3.connect(dbName)
+	c = self.conn.cursor()
+	c.execute('CREATE TABLE IF NOT EXISTS userTable(userId INTEGER NOT NULL UNIQUE, userName TEXT NOT NULL, chatID TEXT)')
 
 def main():
-
-	bot = telebot.TeleBot('269890633:AAHaYy-FkcZwm1ooxtMmfTJ3yIhjbnfNe2E', skip_pending=True)
-	pruebas_disponibles = []
 
 	@bot.message_handler(commands=['start'])
 	def send_welcome(message):
@@ -37,11 +44,14 @@ def main():
 
 	@bot.message_handler(commands=['help'])
 	def send_help(message):
-		reply = "Los comandos que puedes usar son: \n - \n - \n -"
+		tmp_list = ""
+		for x in range(0,10):
+			tmp_list += "\n - " + str(pruebas_disponibles[x])
+		reply = "Los comandos que puedes usar son: \n" + tmp_list
 		f = open('log','a')
 		now = datetime.now()
 		f.writelines("***************\n")
-		f.writelines("[%s/%s/%s - %s:%s:%s] /start\n" % (now.day, now.month, now.year, now.hour, now.minute, now.second) )
+		f.writelines("[%s/%s/%s - %s:%s:%s] /help\n" % (now.day, now.month, now.year, now.hour, now.minute, now.second) )
 		f.writelines("[%s/%s/%s - %s:%s:%s] "  % (now.day, now.month, now.year, now.hour, now.minute, now.second) + reply + "\n")
 		f.close
 		bot.send_message(message.chat.id, reply)
@@ -56,6 +66,29 @@ def main():
 		f.writelines("[%s/%s/%s - %s:%s:%s] "  % (now.day, now.month, now.year, now.hour, now.minute, now.second) + reply + "\n")
 		f.close
 		bot.send_message(message.chat.id, reply)
+
+		@bot.message_handler(commands=['prueba1'])
+		def send_help(message):
+			reply = "De momento, no has cumplimentado ninguna prueba: \n"
+			f = open('log','a')
+			now = datetime.now()
+			f.writelines("***************\n")
+			f.writelines("[%s/%s/%s - %s:%s:%s] /start\n" % (now.day, now.month, now.year, now.hour, now.minute, now.second) )
+			f.writelines("[%s/%s/%s - %s:%s:%s] "  % (now.day, now.month, now.year, now.hour, now.minute, now.second) + reply + "\n")
+			f.close
+			bot.send_message(message.chat.id, reply)
+
+	@bot.message_handler(commands=['progreso'])
+	def send_help(message):
+		reply = "De momento, no has cumplimentado ninguna prueba: \n"
+		f = open('log','a')
+		now = datetime.now()
+		f.writelines("***************\n")
+		f.writelines("[%s/%s/%s - %s:%s:%s] /start\n" % (now.day, now.month, now.year, now.hour, now.minute, now.second) )
+		f.writelines("[%s/%s/%s - %s:%s:%s] "  % (now.day, now.month, now.year, now.hour, now.minute, now.second) + reply + "\n")
+		f.close
+		bot.send_message(message.chat.id, reply)
+
 
 	@bot.message_handler(content_types=['photo'])
 	def photo(*mensaje):
@@ -75,9 +108,9 @@ def main():
 			downloaded_file = bot.download_file(file_info.file_path)
 			thing = io.BytesIO(downloaded_file)
 			thing.name = raw + "png"
-			bot.send_photo("57311471", thing)
+			bot.send_photo(chat_id, thing)
 
-	@bot.message_handler(content_types=['audio'])
+	@bot.message_handler(content_types=['voice'])
 	def audio(*mensaje):
 		for m in mensaje:
 			chat_id = m.chat.id
@@ -90,13 +123,12 @@ def main():
 			f.writelines("[%s/%s/%s - %s:%s:%s] \n" % (now.day, now.month, now.year, now.hour, now.minute, now.second) + " El usuario "+ user + " ha enviado un audio." )
 			f.close
 
-			raw = m.audio[-1].file_id
+			raw = m.voice.file_id
 			file_info = bot.get_file(raw)
 			downloaded_file = bot.download_file(file_info.file_path)
 			thing = io.BytesIO(downloaded_file)
-			thing.name = raw + "mp3"
-			bot.send_photo("57311471", thing)
-
+			thing.name = raw + "ogg"
+			bot.send_voice(chat_id, thing)
 
 	@bot.message_handler(func=lambda m: True)
 	def save_on_log(message):
